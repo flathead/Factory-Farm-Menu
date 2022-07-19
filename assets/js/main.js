947c7c16b41740ccb304e5f602848be980539e9a
Dmitry <69222296+flathead@users.noreply.github.com>
2022-07-19 21:09:16 +0300

    0.9.1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,16 @@
-jQuery( function( $ ) {
-	$( '.farm-item' ).on( 'mouseenter', function() {
-		$( '.clouds' ).fadeOut(300);
-	} );
-	$( '.farm-container' ).on( 'mouseleave', function() {
-			console.log('Farm zone cursor leave...');
-			setTimeout( function() {
-				$( '.clouds' ).fadeIn(1000);
-			}, 1000);
-	} );
-	$( '.farm-item img' ).on( 'load', function() {
-		$( this ).closest( '.farm-container' ).addClass( 'loaded' );
-	} );
-} );
+jQuery(function ($) {
+	$('.farm-item').on('mouseenter', function () {
+		$( 'Hide the clouds!' )
+		$('.clouds').fadeOut(300);
+	});
+	$('.farm-container').on('mouseleave', function () {
+		console.log('Farm zone cursor leave...');
+		setTimeout(function () {
+			$('.clouds').fadeIn(1000);
+		}, 1000);
+	});
+	$('.farm-item img').waitForImages( function () {
+		console.log( 'All farm images done!' );
+		$('.farm-container').addClass('loaded');
+	});
+});
